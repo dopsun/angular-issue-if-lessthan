aac87c5a7fb35147d57fd6eee897690a992b0f1a
dopsun <sunht@dopsun.com>
2024-07-23 10:21:42 +0800

    reproduce issue

---
+++ issue-if-lessthan/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  readonly viewModel = {
+    total_count: 0
+  };
+
+}
+
+interface ViewModel {
+  total_count: number;
+}
